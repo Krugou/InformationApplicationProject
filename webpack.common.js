607d442d7dcd3c41f378e8783f14c35292f41bf3
Krugou <59641229+Krugou@users.noreply.github.com>
2023-02-11 16:52:51 +0200

    asd

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const ImageWebpackLoader = require('image-webpack-loader');
 module.exports = {
   entry: {
     jak: './src/index.js',
@@ -17,6 +18,10 @@ module.exports = {
         },
       ]
     }),
+    new ImageminWebpackPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+    }),
+
     new HtmlWebpackPlugin({
       title: 'Info App Project',
       meta: {
@@ -56,7 +61,43 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         type: 'asset/resource'
-      }
-    ]
-  }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+          {
+            loader: ImageWebpackLoader,
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      },
+    ],
+  },
+
 };
