5b8128e121421847943343e5a62890cec38f1f93
Krugou <59641229+Krugou@users.noreply.github.com>
2023-02-24 15:13:40 +0200

    Update hsl-render.js

--- src/js/modules/hsl/hsl-render.js
+++ src/js/modules/hsl/hsl-render.js
@@ -2,26 +2,32 @@ import HSL from './hsl-data';
 
 const renderHSLData = async (target, stop) => {
   try {
-  const routes = await HSL.getRoutesByStopId(stop);
-  console.table('🚀 ~ file: hsl-render.js:5 ~ renderHSLData ~ routes:', routes);
+    const routes = await HSL.getRoutesByStopId(stop);
+    console.table('🚀 ~ file: hsl-render.js:5 ~ renderHSLData ~ routes:', routes);
 
-  const ul = document.createElement('ul');
-  for (const route of routes) {
-    const li = document.createElement('li');
-    const transitLine = document.createElement('p');
-    transitLine.textContent = `linja: ${route.name}`;
-    const arrivalTime = document.createElement('p');
-    arrivalTime.textContent = `saapuu: ${route.realtimeArrival}`;
-    const transitLineDirection = document.createElement('p');
-    transitLineDirection.textContent = `suunta: ${route.headsign}`;
+    const hslContainer = document.createElement('ul');
+    hslContainer.classList.add('hsl-container');
+    for (const route of routes) {
+      const hslContainerList = document.createElement('li');
+      hslContainerList.classList.add('hsl-container-list');
 
-    li.append(transitLine,transitLineDirection,  arrivalTime);
-    ul.append(li);
+      const transitLine = document.createElement('p');
+      transitLine.textContent = `linja: ${route.name}`;
+      transitLine.classList.add('transit-line');
+      const arrivalTime = document.createElement('p');
+      arrivalTime.textContent = `saapuu: ${route.realtimeArrival}`;
+      arrivalTime.classList.add('arrival-time');
+      const transitLineDirection = document.createElement('p');
+      transitLineDirection.textContent = `suunta: ${route.headsign}`;
+      transitLineDirection.classList.add('transit-line-direction');
+
+      hslContainerList.append(transitLine, transitLineDirection, arrivalTime);
+      hslContainer.append(hslContainerList);
+    }
+    target.append(hslContainer);
+  } catch (error) {
+    console.error(error);
   }
-  target.append(ul);
-} catch (error) {
-  console.error(error);
-}
 };
 const hslRender = { renderHSLData };
 
