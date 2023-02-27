18af58ff099a0dd3d5b79bb42aa6fce5a38227db
Krugou <59641229+Krugou@users.noreply.github.com>
2023-02-27 06:35:19 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -269,12 +269,38 @@ const getStopsNearbyHsl = async () => {
     hsl.innerHTML = 'No restaurant found';
   }
 };
+const renderVideo = (target) => {
+  //  <video autoplay muted controls loop class="video" src='./assets/videos/infodisplay10sec.mp4'></video>
+  const video = document.createElement('video');
+  video.autoplay = true;
+  video.muted = true;
+  video.controls = true;
+  video.loop = true;
+  video.classList.add('video');
+  video.src = './assets/videos/infodisplay10sec.mp4';
+  target.append(video);
+};
+const hslContainer = (target) => {
+  // <section class="hsl"> <h2>HSL</h2> <div class="hsl-list"></div></section >;
 
+  const hsl = document.createElement('section');
+  hsl.classList.add('hsl');
+  const h2 = document.createElement('h2');
+  h2.textContent = 'HSL';
+  const hslList = document.createElement('div');
+  hslList.classList.add('hsl-list');
+  hsl.append(h2);
+  hsl.append(hslList);
+  target.append(hsl);
+};
 const leftside = document.querySelector('.leftside');
 const initiate = async () => {
+  paSystem.getAnnouncements(leftside);
+  renderVideo(leftside);
+  hslContainer(leftside);
   fetchWeatherLocalorDefault(1);
   getStopsNearbyHsl();
   renderMenu();
-  paSystem.getAnnouncements(leftside);
+
 };
 initiate();
