6954bf1e41060dfb769ae0a678a201431b896cb3
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-01 17:18:54 +0200

    Update sodexoMenu.js

--- src/js/modules/restaurant/sodexoMenu.js
+++ src/js/modules/restaurant/sodexoMenu.js
@@ -16,13 +16,14 @@ const weeklyUrl = 'https://www.sodexo.fi/ruokalistat/output/weekly_json/';
 const getDailyMenu = async (restaurantId) => {
   try {
 
-  const weeklyMenu = await doFetch(weeklyUrl + restaurantId);
-  const menu = weeklyMenu.mealdates[getWeekdayIndex()];
-  if (menu === undefined) {
-    alert('no Sodexo data for today, showing past fridays data');
-    return weeklyMenu.mealdates[4];
-  }
-  return menu;
+    const weeklyMenu = await doFetch(weeklyUrl + restaurantId);
+    const menu = weeklyMenu.mealdates[getWeekdayIndex()];
+    console.log('🚀 ~ file: sodexoMenu.js:21 ~ getDailyMenu ~ menu:', menu);
+    if (menu === undefined) {
+      alert('no Sodexo data for today, showing past fridays data');
+      return weeklyMenu.mealdates[4];
+    }
+    return menu;
   } catch (error) {
     console.error('getDailyMenu error', error);
   }
@@ -35,20 +36,24 @@ const getDailyMenu = async (restaurantId) => {
  * @param {*} lang selected language for meal titles
  * @returns meal names, price and allergies or a 'nodata' menu if data is undefined (this means API fetch failed)
  */
-const parseMenu = (menu, lang) =>  {
-  if (menu === undefined){
+const parseMenu = (menu, lang) => {
+  if (menu === undefined) {
     const failedFetch = [];
     return failedFetch[0] = ['no data'];
   }
 
   const coursesEn = Object.values(menu.courses).map((course) => {
-     return course.title_en + '|('+ course.dietcodes + ')|' + course.price; }
-     );
+    console.log('🚀 ~ file: sodexoMenu.js:48 ~ coursesEn ~ course:', course);
+
+    return course.title_en + '|(' + course.dietcodes + ')|' + course.price;
+  }
+  );
   const coursesFi = Object.values(menu.courses).map((course) => {
-    console.log(course);
-  return course.title_fi + '|('+ course.dietcodes + ')|' + course.price; });
+    console.log('🚀 ~ file: sodexoMenu.js:49 ~ coursesFi ~ course:', course);
+    return course.title_fi + '|(' + course.dietcodes + ')|' + course.price;
+  });
   return lang === 'en' ? coursesEn : coursesFi;
 };
 
-const sodexoMenu = {getDailyMenu, parseMenu};
+const sodexoMenu = { getDailyMenu, parseMenu };
 export default sodexoMenu;
