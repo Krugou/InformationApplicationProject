1770226668c545341098c04bb0e3a3233b910a37
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-03 22:35:40 +0200

    Update sw.js

--- src/js/modules/utils/sw.js
+++ src/js/modules/utils/sw.js
@@ -1,10 +1,15 @@
 const serviceWorkerFunction = () => {
   // eslint-disable-next-line no-undef
+  // Check if service workers are supported
   if (APP_CONF.productionMode && 'serviceWorker' in navigator) {
+    // If supported, add load event listener
     window.addEventListener('load', () => {
+      // In event listener, register service worker
       navigator.serviceWorker.register('./service-worker.js').then(registration => {
+        // If registration successful, log it to the console
         console.log('SW registered: ', registration);
       }).catch(registrationError => {
+        // If there are any errors, log them to the console
         console.log('SW registration failed: ', registrationError);
       });
     });
