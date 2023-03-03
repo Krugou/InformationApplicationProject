a6e5b76b32cc5942155e9d03c3ab05a196e43682
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-03 22:26:32 +0200

    Update campusinfo.js

--- src/js/modules/utils/campusinfo.js
+++ src/js/modules/utils/campusinfo.js
@@ -6,11 +6,20 @@ import campusInfo from '../../../../json/campuses.json';
  */
 const allCampuses = campusInfo.campuses;
 
+// Create a function that takes the selectedCampus as a parameter
 const getCampusInfo = (selectedCampus) => {
+  // Create a new variable that stores the filtered results of the allCampuses array
   const currentCampusInfo = allCampuses.filter((restaurant) => {
+    // Loop through the allCampuses array and return only the campus that matches the selectedCampus
     return restaurant.name === selectedCampus;
-  })[0];
-  return currentCampusInfo;
+  });
+  // If the currentCampusInfo array has a length greater than 0, return the first item in the array
+  if (currentCampusInfo.length > 0) {
+    return currentCampusInfo[0];
+  } else {
+    // If the currentCampusInfo array has a length of 0, return false
+    return false;
+  }
 };
 
 export default getCampusInfo;
