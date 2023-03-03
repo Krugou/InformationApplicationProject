1ceb04b25b4ebba844abf560aa206c96afcaca55
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-04 00:48:49 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 // jest.config.js on Jest - testikehyksen konfiguraatiotiedosto, joka sisältää testejä koskevat asetukset, kuten testien etsintäpolun, testattavan koodin polun, testiympäristön määrittelyt, tiedostojen ja testattavien koodinpätkien poissulkemiset sekä monia muita vaihtoehtoja.Jest käyttää jest.config.js - tiedostoa lukeakseen testausasetukset ja mukauttaakseen testiympäristön tarpeisiisi.
 
-module.exports = {
-
-
-
-  // Define any additional Jest configuration options you need
-  // For example, you can configure Jest to only look for tests in specific directories
-  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)']
+export default {
+  'extensionsToTreatAsEsm': ['.js'],
+  'testEnvironment': 'node',
+  'transform': {},
+  'transformIgnorePatterns': [
+    '/node_modules/(?!(@babel/runtime|lodash-es|other-es-module)/)',
+  ],
 };
+
