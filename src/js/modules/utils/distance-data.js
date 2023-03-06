029790f390d4bf1a1d25981f0f47b1fbec54919b
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-06 04:14:38 +0200

    etäisyys pinni

--- src/js/modules/utils/distance-data.js
+++ src/js/modules/utils/distance-data.js
@@ -44,10 +44,10 @@ const getUserlocation = (campus) => {
       const h2 = document.createElement('h2');
       h2.classList.add('distance');
       // create textnode
-      h2.textContent = ` ${distanceRounded} M`;
+      h2.innerHTML = ` ${distanceRounded} M <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">   <path fill="ff5000" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z"/></svg>
+`;
       // append p to rightside
       rightSide.prepend(h2);
-      console.log('🚀 ~ file: distance-data.js:53 ~ navigator.geolocation.getCurrentPosition ~ distance:', distanceRounded);
 
 
     }
