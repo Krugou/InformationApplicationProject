9f276bef3a57ff275aa1e5b79c77481bc21655a3
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-03-07 22:41:00 +0200

    sw javadoc

--- src/js/modules/utils/sw.js
+++ src/js/modules/utils/sw.js
@@ -1,9 +1,12 @@
-/**
- * @fileoverview Service Worker registration
+'use strict';
+/** Service worker module
+ *  @module sw
+ */
+/** Registers serviceworkers in production mode
  *
  */
 const serviceWorkerFunction = () => {
-  
+
   // eslint-disable-next-line no-undef
   if (APP_CONF.productionMode && 'serviceWorker' in navigator) {
     // If supported, add load event listener
